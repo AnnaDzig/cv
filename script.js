cccb6629b0af325538c92e5f24a9e202575a0bda
AnnaDzig <adzyhota@protonmail.com>
2022-10-18 11:46:59 +0200

    feat: add about me and rictures of works

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ document.querySelectorAll(".topnav__link").forEach(n => n. addEventListener("cli
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+function myFunction() {
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("more");
+    const btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
